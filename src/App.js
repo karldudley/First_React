d38792f4f5ddfe765d2bac34b010103774f4e537
karl dudley <karldudley@gmail.com>
2022-09-27 14:16:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'Favourite Music'
+  const likes = 50
+  const link = "http://www.google.com"
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+        {/* <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ "Hey" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random()*10 }</p>
+        <a href={link}>Google</a> */}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
